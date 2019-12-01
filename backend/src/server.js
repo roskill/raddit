aa19bdb68940410d5fbafa3692a304c28383a323
roskill <roskill76@gmail.com>
2019-12-01 22:15:40 +1100

    Use middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", (req, res) => res.send("Hello"));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
