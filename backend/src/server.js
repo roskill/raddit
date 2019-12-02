80e6aa362d85693f11531f12f6b08d5225700e57
roskill <roskill76@gmail.com>
2019-12-02 18:48:29 +1100

    Add more sanity test routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,5 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get("/hello", (req, res) => res.send("Hello"));
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
+app.get("/posts/:postId", (req, res) =>
+  res.send(`Fetching post ${req.params.postId}`)
+);
 
 app.listen(8000, () => console.log("Listening on port 8000"));
