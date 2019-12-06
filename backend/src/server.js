f18e40eb4490cd254f15e9bb5e31725830186b04
roskill <roskill76@gmail.com>
2019-12-06 22:50:59 +1100

    Add comment route

--- backend/src/server.js
+++ backend/src/server.js
@@ -44,4 +44,28 @@ app.post('/api/posts/:postId/upvote', async (req, res) => {
   }
 });
 
+app.post('/api/posts/:postId/add-comment', async (req, res) => {
+  try {
+    const { userName, text } = req.body;
+    const postId = req.params.postId;
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+    });
+    const db = client.db('raddit');
+    const post = await db.collection('posts').findOne({ postId: postId });
+    post.comments.push({ userName, text });
+    await db.collection('posts').updateOne(
+      { postId: postId },
+      {
+        $set: { comments: post.comments },
+      },
+    );
+    const updatedPost = await db.collection('posts').findOne({ postId: postId });
+    res.status(200).json(updatedPost);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));
