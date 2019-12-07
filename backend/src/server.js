8c94a348b42dcb067c08adc1a05b64b7c0ae0838
roskill <roskill76@gmail.com>
2019-12-08 10:37:56 +1100

    Add route for add post

--- backend/src/server.js
+++ backend/src/server.js
@@ -44,6 +44,22 @@ app.post('/api/posts/:postId/upvote', async (req, res) => {
   }
 });
 
+app.post('/api/posts/add-post', async (req, res) => {
+  try {
+    const { postId, title, text } = req.body;
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser: true,
+    });
+    const db = client.db('raddit');
+    await db.collection('posts').insertOne({ postId, title, text, upvotes: 0, comments: [] });
+    const updatedPost = await db.collection('posts').findOne({ postId });
+    res.status(200).json(updatedPost);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+});
+
 app.post('/api/posts/:postId/add-comment', async (req, res) => {
   try {
     const { userName, text } = req.body;
